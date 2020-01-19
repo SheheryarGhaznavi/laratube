6e8293a317ae70f355144ae1a7dc205d5b2c667d
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2020-01-19 17:25:39 +0500

    seeder developed :yum:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Laratube\Models\Channel;
+use Laratube\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user1 = factory(User::class)->create([
+            'email' => 'user1@gmail.com'
+        ]);
+        $user2 = factory(User::class)->create([
+            'email' => 'user2@gmail.com'
+        ]);
+
+
+        $channel1 = $user1->channel()->create([
+            'name' => $user1->name
+        ]);
+        $channel2 = $user2->channel()->create([
+            'name' => $user2->name
+        ]);
+
+
+        // $channel1 = factory(Channel::class)->create([
+        //     'user_id' => $user1->id
+        // ]);
+        // $channel2 = factory(Channel::class)->create([
+        //     'user_id' => $user2->id
+        // ]);
+
+
+        $channel1->subscription()->create([
+            'user_id' => $user2->id
+        ]);
+        $channel2->subscription()->create([
+            'user_id' => $user1->id
+        ]);
     }
 }
